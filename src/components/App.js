cbdf98efbc8c3429b1dce0056e68ea5fe0dacc03
mbalsamo <github email address>
2018-05-16 11:12:16 -0400

    done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+// const App = () => <FruitBasket />;
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fruit: [],
+      filters: [],
+      currentFilter: null
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
 
 export default App;
